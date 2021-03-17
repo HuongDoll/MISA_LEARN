c0c7a069f2fb315c0937a87d6b276c7a623be5e1
Hương Doll <bebong11091999@gmail.com>
2021-03-17 12:00:48 +0700

    ...

--- HTML&CSS/js/main.js
+++ HTML&CSS/js/main.js
@@ -1,9 +1,23 @@
-var isEdit;
-var editid;
+var isEdit; // xác định thay đổi thông tin khách hàng hay thêm mới khách hàng
+var selectid; // lưu trữ id khách hàng
 $(document).ready(function() {
-    loaddata();
+    // load thông tin khách hàng vào table
+    loaddata(); 
+
+    // ẩn dialog nhập thông tin khách hàng
     $('#dialog').hide();
     $('#dialog-child').hide();
+
+    // ẩn dialog tùy chọn sửa xóa thông tin khách hàng
+    $('#mini-dialog').hide();
+    $('#mini-select').hide();
+
+    $('#mini-dialog').click(function(){
+        $('#mini-dialog').hide();
+        $('#mini-select').hide();
+    })
+
+    // action cho button thêm khách hàng
     $('#add-employee').click(function() {
         isEdit = false;
         $('#dialog').show();
@@ -20,50 +34,73 @@ $(document).ready(function() {
             $('#CompanyTaxCode').val(""),
             $('#CustomerGroupName').val("")
     })
+    // button tắt dialog
     $('#close-dialog').click(function() {
         $('#dialog').hide();
         $('#dialog-child').hide();
     })
+    // tắt dialog khi ấn ra ngoài form
     $('#dialog').click(function() {
         $('#dialog').hide();
         $('#dialog-child').hide();
     })
+    // button hủy 
     $('#button-delete').click(function() {
         $('#dialog').hide();
         $('#dialog-child').hide();
     })
+    // button lưu thông tin
     $('#button-save').click(function() {
         if (isEdit) editdata();
         else savedata();
     })
+    // action khi double click vào 1 phần tử trong bảng
     $('table tbody').on('dblclick', 'tr', function() {
-        var id = $(this).attr('recordId');
-        deleteItem(id);
+        selectid = $(this).attr('recordId');
+        console.log($(this).position());
+        var top = $(this).position().top +210;
+        var left = $(this).position().left +120;
+        $('#mini-select').css({top: top, left: left, position:'absolute'});
+        $('#mini-dialog').show();
+        $('#mini-select').show();
     })
-    $('table tbody').on('click', 'tr', function() {
-        var id = $(this).attr('recordId');
-        editItem(id);
+    // action cho button sửa thông tin khách hàng
+    $('#button-edit').click(function(){
+        $('#mini-dialog').hide();
+        $('#mini-select').hide();
+        editItem(selectid);
     })
-});
+    // action cho button xóa thông tin khách hàng
+    $('#button-delete-item').click(function(){
+        $('#mini-dialog').hide();
+        $('#mini-select').hide();
+        if(confirm("Bạn chắc chắn muốn xóa thông tin khách hàng???")){
+            deleteItem(selectid);
+        }
+    })
 
-function editItem(id) {
+});
+// function sửa khách hàng
+function editItem(selectid) {
     isEdit = true;
-    editid = id;
+    //lấy thông tin khách hàng 
     $.ajax({
-        url: 'http://api.manhnv.net/api/customers/' + id,
+        url: 'http://api.manhnv.net/api/customers/' + selectid,
         method: "GET"
     }).done(function(result) {
-        $('#CustomerCode').val(result.CustomerCode),
-            $('#FullName').val(result.FullName),
-            $('#Gender').val(result.Gender),
-            $('#Address').val(result.Address),
-            $('#DateOfBirth').val(result.DateOfBirth),
-            $('#Email').val(result.Email),
-            $('#PhoneNumber').val(result.PhoneNumber),
-            $('#MemberCardCode').val(result.MemberCardCode),
-            $('#CompanyName').val(result.CompanyName),
-            $('#CompanyTaxCode').val(result.CompanyTaxCode),
-            $('#CustomerGroupName').val(result.CustomerGroupName)
+        console.log(result);
+            $('#CustomerCode').val(result.CustomerCode);
+            $('#FullName').val(result.FullName);
+            $('#Gender').val(result.Gender);
+            $('#Address').val(result.Address);
+            $('#DateOfBirth').val(result.DateOfBirth);
+            $('#Email').val(result.Email);
+            $('#PhoneNumber').val(result.PhoneNumber);
+            $('#MemberCardCode').val(result.MemberCardCode);
+            $('#CompanyName').val(result.CompanyName);
+            $('#CompanyTaxCode').val(result.CompanyTaxCode);
+            $('#CustomerGroupName').val(result.CustomerGroupName);
+        // hiển thị hộp thoại sửa thông tin
         $('#dialog').show();
         $('#dialog-child').show();
 
@@ -71,14 +108,14 @@ function editItem(id) {
 
     })
 }
-
+// sửa thông tin khách hàng trong data
 function editdata() {
     var customer = getCustomer();
-    customer.CustomerId = editid;
+    customer.CustomerId = selectid;
     console.log(customer);
     if (validateCustomer(customer) && customer.CustomerId != "") {
         $.ajax({
-            url: 'http://api.manhnv.net/api/customers/' + editid,
+            url: 'http://api.manhnv.net/api/customers/' + selectid,
             method: "PUT",
             data: JSON.stringify(customer),
             contentType: "application/json"
@@ -92,7 +129,7 @@ function editdata() {
         })
     }
 }
-
+// xóa thông tin khách hàng dựa theo id
 function deleteItem(id) {
     $.ajax({
         url: 'http://api.manhnv.net/api/customers/' + id,
@@ -104,7 +141,7 @@ function deleteItem(id) {
 
     })
 }
-
+// lưu thông tin khách hàng nhập từ form
 function savedata() {
     var customer = getCustomer();
     if (validateCustomer(customer)) {
@@ -123,7 +160,7 @@ function savedata() {
         })
     }
 }
-
+// validate thông tin nhập vào từ form
 function validateCustomer(customer) {
     if (customer.CustomerCode == "") {
         alert("Mã khách hàng không được để trống!");
@@ -142,7 +179,7 @@ function validateCustomer(customer) {
     }
     return true;
 }
-
+// lấy thông tin nhập từ form
 function getCustomer() {
     var customer = {
         "CustomerCode": $('#CustomerCode').val(),
@@ -159,7 +196,29 @@ function getCustomer() {
     }
     return customer;
 }
+// format lại ngày tháng năm sinh
+function formatDate(datee){
+    var date = new Date(datee);
+    var year = date.getFullYear();
 
+    var month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+  
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+    
+    return month + '/' + day + '/' + year;
+}
+// format lại giới tính
+function formatGender(gender){
+    if(gender == 1) return "Nam";
+    else {
+        if(gender == 2) return "Nữ";
+        else return "Khác";
+    }
+}
+
+// tải thông tin khách hàng từ api
 function loaddata() {
     $.ajax({
         url: "http://api.manhnv.net/api/customers",
@@ -169,12 +228,12 @@ function loaddata() {
         for (var i = 0; i < data.length; i++) {
             var info = $(`<tr recordId="${data[i].CustomerId}">
                 <td class="wight-2">${data[i].CustomerCode}</td>
-                <td class="wight-3">${data[i].FullName}</td>
-                <td class="wight-1">${data[i].Gender}</td>
-                <td class="wight-1">${data[i].DateOfBirth}</td>
+                <td class="wight-2">${data[i].FullName}</td>
+                <td class="wight-1">${formatGender(data[i].Gender)}</td>
+                <td class="wight-2">${formatDate(data[i].DateOfBirth)}</td>
                 <td class="wight-2">${data[i].CustomerGroupName}</td>
                 <td class="wight-1">${data[i].PhoneNumber}</td>
-                <td class="wight-3">${data[i].Email}</td>
+                <td class="wight-2">${data[i].Email}</td>
                 <td class="wight-3">${data[i].Address}</td>
                 <td class="wight-1">${data[i].MemberCardCode}</td>
             </tr>`);
